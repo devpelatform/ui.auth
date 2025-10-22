b96f5b0eaf8a3fe461fcb3321c4069d1079d971c
Pelatform Dev <pelatformdev@gmail.com>
2025-10-22 17:05:45 +0700

    chore(deps): update dependencies and configuration for compatibility with Next.js 16
    - Upgraded `@biomejs/biome` to version 2.2.7 and updated related schema in `biome.json`.
    - Updated `next-intl` to version 4.4.0 and adjusted its peer dependencies for Next.js 16 compatibility.
    - Refactored imports in documentation and example projects to align with new package structures.
    - Enhanced `next.config.mjs` with experimental features for improved development experience.
    - Updated various dependencies across projects to ensure compatibility and performance improvements.

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -4,6 +4,11 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  // nextjs 16
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+
   reactStrictMode: true,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/ui.auth' : '',
